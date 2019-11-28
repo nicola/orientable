5ea1496e422feedc31c105a3f2587ca65f0419ae
nicola <nicola@users.noreply.github.com>
2019-11-28 12:04:03 -0800

    adding over described

--- bin.js
+++ bin.js
@@ -12,12 +12,30 @@ require('yargs')
       })
     },
     (argv) => {
-      console.log('Missing descriptions for: ')
       const raw = fs.readFileSync(argv.path, 'utf-8')
       const model = orient.parse(raw)
+
       const undescribed = Object.keys(model.descriptions).filter(k => model.descriptions[k].length === 0).sort()
-      console.log(undescribed.join('\n'))
-      console.log(`\nMissing descriptions for ${undescribed.length} terms`)
+      const overdescribed = Object.keys(model.descriptions).filter(k => model.descriptions[k].length > 1).sort()
+
+      // Listing undescribed
+      console.log(undescribed.map(d => `Missing describe: ${d}`).join('\n'))
+      console.log(overdescribed.map(d => `Too many describe: ${d}`).join('\n'))
+
+      let ok = true
+      if (undescribed.length > 0) {
+        console.log(`- Missing descriptions for ${undescribed.length} terms`)
+        ok = false
+      }
+      if (overdescribed.length > 0) {
+        console.log(`- Too many describe for ${overdescribed.length} terms`)
+        ok = false
+      }
+
+      if (ok) {
+        console.log('All good!')
+        return 0
+      }
     }
   )
   .command(
